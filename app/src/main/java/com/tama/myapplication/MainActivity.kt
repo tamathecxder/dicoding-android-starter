e5769bf3ec9b13458af8396364b7da710d5a5048
tamathecxder <yudistiraeka.pratama012@gmail.com>
2024-10-06 00:11:43 +0700

    setup the variables

--- app/src/main/java/com/tama/myapplication/MainActivity.kt
+++ app/src/main/java/com/tama/myapplication/MainActivity.kt
@@ -2,8 +2,17 @@ package com.tama.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var edtWidth: EditText
+    private lateinit var edtHeight: EditText
+    private lateinit var edtLength: EditText
+    private lateinit var btnCalculate: Button
+    private lateinit var tvResult: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
